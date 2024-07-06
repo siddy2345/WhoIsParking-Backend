af18984cd1fdb51d0e6a4c494d867fadd983283e
Sahid Fakiry <s.fakiry23@gmail.com>
2024-07-06 20:04:32 +0200

    feat(WhoIsParking): add models for db

---
+++ Domain.WhoIsParking/Models/House.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.WhoIsParking.Models;
+
+public class House
+{
+    [Required]
+    public int HouseId { get; set; }
+
+    [MaxLength(100), Required]
+    public string Street {  get; set; } = string.Empty;
+
+    [MaxLength(20), Required]
+    public string Number { get; set; } = string.Empty;
+
+    [Required]
+    public int PLZ { get; set; }
+
+    [MaxLength(100), Required]
+    public string City { get; set; } = string.Empty;
+
+    public IEnumerable<ParkedCar> ParkedCars { get; set; } = Enumerable.Empty<ParkedCar>();
+}
---
+++ Domain.WhoIsParking/Models/ParkedCar.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.WhoIsParking.Models;
+
+public class ParkedCar
+{
+    [Required]
+    public int ParkedCarId { get; set; }
+
+    [MaxLength(20), Required]
+    public string NumberPlate { get; set; } = string.Empty;
+
+    [MaxLength(100), Required]
+    public string CarBrand { get; set; } = string.Empty;
+
+    [MaxLength(100), Required]
+    public string CarModel { get; set; } = string.Empty;
+
+    [MaxLength(100), Required]
+    public string Firstname { get; set; } = string.Empty;
+
+    [MaxLength(100), Required]
+    public string Lastname { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime Arrival { get; set; }
+
+    [Required]
+    public int HouseId { get; set; }
+
+    public House? House { get; set; }
+}
