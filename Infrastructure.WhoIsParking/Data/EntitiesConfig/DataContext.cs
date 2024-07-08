a94ffb6033aa19bfd6ad94bf02f698e64428d364
sahid <s.fakiry23@gmail.com>
2024-07-08 09:03:04 +0200

    feat(WhoIsParking): add DataContext

---
+++ Infrastructure.WhoIsParking/Data/EntitiesConfig/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.WhoIsParking.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.WhoIsParking.Data.EntitiesConfig;
+
+public class DataContext : IdentityDbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<House> House { get; set; }
+
+    public DbSet<ParkedCar> ParkedCar { get; set; }
+}
