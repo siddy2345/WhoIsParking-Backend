ad587ca6f7f235eece614a0b0b94443afad10c25
Sahid Fakiry <s.fakiry23@gmail.com>
2025-03-01 13:23:48 +0100

    refactor(WIP): change db to postgres and add docker as container tool

---
+++ API.WhoIsParking/Extensions/MigrationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.WhoIsParking.Data.EntitiesConfig;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.WhoIsParking.Extensions;
+
+public static class MigrationExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+        using DataContext dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+        dataContext.Database.Migrate();
+    }
+}
